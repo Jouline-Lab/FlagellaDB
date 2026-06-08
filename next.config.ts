4f6325f90a45531598d72235e6f284d213399807
berkayselcuk <bekoselo@gmail.com>
2026-06-08 08:58:35 -0400

    Repo name changed

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const isGithubActions = process.env.GITHUB_ACTIONS === "true";
 const repositoryName =
-  process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "Flagella-Evolution-Database";
+  process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "FlagellaDB";
 const basePath = isGithubActions ? `/${repositoryName}` : "";
 
 const nextConfig: NextConfig = {
